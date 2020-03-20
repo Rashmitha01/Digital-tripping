e49a4194df896c79f54e627f3ca8284cfee07016
Rashmitha <36674921+Rashmitha01@users.noreply.github.com>
2020-03-20 13:37:07 +0530

    SD card read/write feature added.
    SD Init(), read File(), write File(); methods added and related SD and SPI communication libraries are added
    nothing changed inside the loop and the main functions of the code
    next Step: calibration of sensor and readings of the sensors will be separated from the main loop()

--- ACS712_AC_DC_reading/ACS712_AC_DC_reading.ino
+++ ACS712_AC_DC_reading/ACS712_AC_DC_reading.ino
@@ -1,15 +1,56 @@
 
 #include "ACS712.h"
+#include <SD.h>
+#include<SPI.h>
 
-#define sensorInput A0  // sensor connects to analog 0 
+#define currentSensor A0  // sensor connects to analog 0 
 #define dcMeasure   2   // measure DC/AC when input 2 is HIGH/LOW
 #define calInput    3   // offset calibration performed when input 3 = LOW
 
 // Sensor options:  ACS712_05B = 5 amp, ACS712_20A = 20 amp, ACS712_30A = 30 amp
-ACS712 sensor(ACS712_20A, sensorInput);
+ACS712 sensor(ACS712_20A, currentSensor);
+File ReadingsFile;
+
+void SDInit() {
+  while (!Serial) {
+    ;//wait for port to connect.Needed for native USB port only
+  }
+  Serial.print("Initializing SD card..");
+  if (!SD.begin(4)) {
+    Serial.println("Initialization failed..");
+    while (1);
+  }
+  Serial.println("Initialization DONE.");
+}
+
+void writefile() {
+  //opening the file can be done once at a time.
+  ReadingsFile = SD.open("AmpReadings.txt", FILE_WRITE);
+  if (ReadingsFile) {
+    Serial.print("writing to ampRaedings file..")
+    ReadingsFile.println(analogRead(currentSensor));
+  } else {
+    Serial.println("error opening in AmpReadings.txt");
+  }
+}
+
+void readfile() {
+  //re-open the file for redaing
+  ReadingsFile = SD.open("AmpReadings.txt");
+  if (AmpReadings) {
+    Serial.println("AmpReadings.txt opened.");
+    Serial.write(ReadingsFile.read());
+    //-->also we need to get a return fron here
+    //-->String ampReadingString = readingsfile.read();
+  }
+  ReadingsFile.close();
+} else {
+  Serial.println("erroe opening AmpReadings.txt");
+}
 
 void setup() {
   Serial.begin(9600);
+  SDInit();
 
   pinMode (dcMeasure, INPUT_PULLUP);
   pinMode (calInput, INPUT_PULLUP);
